cc6e4d6ef1dd19619e25dea5dd326cc8a1a79d61
SadiqRehmetov <friyaziyyat@gmail.com>
2024-02-07 00:30:52 +0400

    final

---
+++ assets/js/archive_course.js
@@ -0,0 +1,169 @@
+let nav = document.querySelector("header")
+let searchIcon = document.querySelector("#search")
+let searchInput = document.querySelector("#searchinput")
+let menuİcon = document.querySelector(".bi-list")
+let responsMenu = document.querySelector(".respons-menu")
+let closeMenu = document.querySelector(".closemenu")
+let courseCarts = document.querySelector(".course-carts")
+let searcInput = document.querySelector(".search")
+let arr_1=[]
+let arr_2=[]
+let sort = document.querySelector("#sort")
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        nav.style.position = "fixed";
+        nav.style.transition = "position 0.3s ease"; 
+    } else {
+        nav.style.position = "static"; 
+        nav.style.transition = "position 0.3s ease";
+    }
+});
+searchIcon.addEventListener("click", () => {
+    searchInput.classList.toggle("search");
+    searchInput.classList.toggle("searchInput");
+    if(menuİcon.style.display=="none"){
+        menuİcon.style.display="flex";
+    }
+    else{
+        menuİcon.style.display="none";
+        
+    }
+});
+menuİcon.addEventListener("click",()=>{
+    responsMenu.style.transform = "translateX(0)";
+})
+closeMenu.addEventListener("click", ()=>{
+    responsMenu.style.transform = "translateX(-500%)";
+})
+
+showCourseData()
+function showCourseData(){
+    fetch(`http://localhost:3000/course`)
+    .then(res=>res.json())
+    .then(respons=>{
+        arr_2=respons;
+        courseCarts.innerHTML="";
+        arr_1=arr_1.length || searcInput.value ? arr_1: respons;
+        arr_1.map((element)=>{
+            courseCarts.innerHTML+=`
+
+            <div class="cart">
+                        <div class="hiddenDiv">
+                            <div class="heartDiv">
+                                <i class="bi bi-heart"></i>
+                            </div>
+                            <span class="title-span">Beginner</span>
+                            <h3>Starting SEO as your Home Based Business</h3>
+                            <p class="ratings">
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <span>(5.0 / 3 Ratings)</span>
+                            </p>
+                            <span class="price">$30</span>
+                            <p class="cart-info">
+                                Lorem ipsum dolor sit amet consectur adipiscing elit, sed do eiusmod tempor
+                            </p>
+                            <div class="icons">
+                                <p>
+                                    <i class="bi bi-journal-richtext"></i>
+                                    <span>11 Lesson</span>
+                                </p>
+                                <p>
+                                    <i class="bi bi-person"></i>
+                                    <span>229 Students</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="image">
+                            <img src="${element.image}" alt="picture">
+                            <div class="date">
+                                <i class="bi bi-clock"></i>
+                                <span>15 weeks</span>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <span class="title-span">Beginner</span>
+                            <h3>${element.name}</h3>
+                            <p class="ratings">
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <span>(5.0 / 3 Ratings)</span>
+                            </p>
+                            <span class="price">$${element.price}</span>
+                            <div class="icons">
+                                <p>
+                                    <i class="bi bi-journal-richtext"></i>
+                                    <span>11 Lesson</span>
+                                </p>
+                                <p>
+                                    <i class="bi bi-person"></i>
+                                    <span>229 Students</span>
+                                </p>
+                            </div>
+                            
+                        </div>
+                    </div>
+
+            `
+        })
+    })
+}
+
+searcInput.addEventListener("input", (e) => {
+    console.log("yhuhu");
+    
+    arr_1 = arr_2;
+    arr_1 = arr_1.filter((element) => {
+      return element.name.toLowerCase().includes(e.target.value.toLowerCase());
+      
+    });
+    showCourseData();
+});
+
+sort.addEventListener("change",(e)=>{
+    if(e.target.value=="exp"){
+      arr_1=arr_1.sort((a,b)=>a.price - b.price)
+    }
+    else if(e.target.value=="cheap"){
+      arr_1=arr_1.sort((a,b)=>b.price - a.price)
+    }
+    else{
+      arr_1=[]
+    }
+    showCourseData()
+  })
+
+
+
+//   sort.addEventListener("change", (e) => {
+//     if (e.target.value == "exp") {
+//         arr_1 = arr_1.sort((a, b) => a.price - b.price);
+//     } else if (e.target.value == "cheap") {
+//         arr_1 = arr_1.sort((a, b) => b.price - a.price);
+//     } else if (e.target.value == "az") {
+//         arr_1 = arr_1.sort((a, b) => {
+//             const nameA = a.name.toLowerCase();
+//             const nameB = b.name.toLowerCase();
+//             if (nameA < nameB) return -1;
+//             if (nameA > nameB) return 1;
+//             return 0;
+//         });
+//     } else if (e.target.value == "za") { 
+//         arr_1 = arr_1.sort((a, b) => {
+//             const nameA = a.name.toLowerCase();
+//             const nameB = b.name.toLowerCase();
+//             if (nameA > nameB) return -1;
+//             if (nameA < nameB) return 1;
+//             return 0;
+//         });
+//     } else {
+//         arr_1 = [];
+//     }
+//     showCourseData();
+// });
--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,8 @@ let menuİcon = document.querySelector(".bi-list")
 let responsMenu = document.querySelector(".respons-menu")
 let closeMenu = document.querySelector(".closemenu")
 let courseCarts = document.querySelector(".course-carts")
+let page = 1;
+let instructorsCarts = document.querySelector(".instructors-carts")
 playIframe.addEventListener("click", ()=>{
     iframeDiv.style.display="flex";
 })
@@ -15,8 +17,6 @@ closeIframe.addEventListener("click",()=>{
     iframeDiv.style.display="none";
     window.location.reload();
 })
-
-
 window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
         nav.style.position = "fixed";
@@ -44,3 +44,98 @@ menuİcon.addEventListener("click",()=>{
 closeMenu.addEventListener("click", ()=>{
     responsMenu.style.transform = "translateX(-500%)";
 })
+
+showData()
+function showData(){
+    fetch(`http://localhost:3000/course?_page=${page}&_limit=4`)
+    .then(res=>res.json())
+    .then(respons=>{
+        respons.map((element)=>{
+            courseCarts.innerHTML+=`
+            <div class="cart">
+                        <div class="hiddenDiv">
+                            <div class="heartDiv">
+                                <i class="bi bi-heart"></i>
+                            </div>
+                            <span class="title-span">${element.category}</span>
+                            <h3>${element.name}</h3>
+                            <p class="ratings">
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <span>(5.0 / 3 Ratings)</span>
+                            </p>
+                            <span class="price">$${element.price}</span>
+                            <p class="cart-info">
+                                ${element.description}
+                            </p>
+                            <div class="icons">
+                                <p>
+                                    <i class="bi bi-journal-richtext"></i>
+                                    <span>11 Lesson</span>
+                                </p>
+                                <p>
+                                    <i class="bi bi-person"></i>
+                                    <span>229 Students</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="image">
+                            <img src="${element.image}" alt="picture">
+                            <div class="date">
+                                <i class="bi bi-clock"></i>
+                                <span>15 weeks</span>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <span class="title-span">${element.category}</span>
+                            <h3>${element.name}</h3>
+                            <p class="ratings">
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <span>(5.0 / 3 Ratings)</span>
+                            </p>
+                            <span class="price">$${element.price}</span>
+                            <div class="icons">
+                                <p>
+                                    <i class="bi bi-journal-richtext"></i>
+                                    <span>11 Lesson</span>
+                                </p>
+                                <p>
+                                    <i class="bi bi-person"></i>
+                                    <span>229 Students</span>
+                                </p>
+                            </div>
+                            
+                        </div>
+                    </div>
+            `
+        })
+    })
+}
+
+showInstructorsData()
+function showInstructorsData(){
+    fetch(`http://localhost:3000/instructors`)
+    .then(res=>res.json())
+    .then(respons=>{
+        respons.map((element)=>{
+            instructorsCarts.innerHTML+=`
+            <div class="cart">
+            <div class="image">
+                <a href="#"><img src="${element.image}" alt="picture"></a>
+            </div>
+            <div class="info">
+                <h3>${element.name}</h3>
+                <p>${element.job}</p>
+            </div>
+            </div>
+            `
+        })
+    })
+}
